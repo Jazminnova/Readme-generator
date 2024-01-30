0e9c2234a615f9c8a93104ee9ec9bb948063717a
jazminnova <jasminnova@gmail.com>
2024-01-29 22:42:22 -0500

    added questions

--- index.js
+++ index.js
@@ -4,7 +4,38 @@ const fs = requirer("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "Give a short discription of your project?",
+    name: "discription"
+},
+{
+    type: "input",
+    message: " Add a Table Of Content",
+    name: "tableofcontent"
+},
+{
+    type: "input",
+    message: "How is this app used",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "What LICENSE did you want to use?",
+    name: "license",
+    license:["MIT","APACHE2","GNU","BOOST","ECLIPSE","MOZILLA"]
+     
+},
+{
+    
+}
+],
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
